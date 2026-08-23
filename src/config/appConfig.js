a578942c35c8b4f23868fa47a535713caa175541
ANDRE LUIZ BARBOSA <andrehlbarbosa@gmail.com>
2026-08-23 05:30:30 -0300

    fix: add fallback values to app config

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
 const appConfig = {
-  port: process.env.PORT,
-  host: process.env.HOST,
-  appName: process.env.APP_NAME,
-  appContext: process.env.APP_CONTEXT,
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || '127.0.0.1',
+  appName: process.env.APP_NAME || 'GC ⚔️ Guerra de Clãs',
+  appContext: process.env.APP_CONTEXT || 'API',
 };
 
 module.exports = appConfig;
